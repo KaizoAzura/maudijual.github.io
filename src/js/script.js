151fa3f940a353733c822bc8639dc2e9aa22fdf8
KaizoAzura <nicolassatria07@gmail.com>
2024-10-20 20:55:30 +0700

    update lagi

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,35 @@ window.addEventListener("scroll", function () {
     previewImage.style.width = "100%";
   }
 });
+
+// slider animation slick
+$(document).ready(function () {
+  function slickInit() {
+    if ($(window).width() < 768) {
+      if (!$(".slider-container").hasClass("slick-initialized")) {
+        $(".slider-container").slick({
+          dots: true,
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: true,
+          autoplaySpeed: 5000,
+          pauseOnHover: true,
+        });
+      }
+    } else {
+      if ($(".slider-container").hasClass("slick-initialized")) {
+        $(".slider-container").slick("unslick");
+      }
+    }
+  }
+
+  // Inisialisasi saat pertama kali load
+  slickInit();
+
+  // Cek saat window di-resize
+  $(window).on("resize", function () {
+    slickInit();
+  });
+});
